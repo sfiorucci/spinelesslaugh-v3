a24236420f1996a115f88f0a5d1b10d3f8db0078
Simone Fiorucci <simone@commercelayer.io>
2023-02-10 10:20:46 +0100

    Refine header logo hover animation

--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -6,9 +6,9 @@
         <!-- Site branding -->
         <div class="shrink-0 mr-4 md:mt-4">
           <!-- Logo -->
-          <router-link class="group flex flex-row" to="/" aria-label="Spineless Laugh">
-            <img src="../images/logo-gradient.svg" class="w-14 transition duration-500 ease-in-out delay-150 hover:rotate-180" alt="Logo" />
-            <div class="text-3xl pl-4 pt-2 transition duration-500 ease-in-out font-bold font-hkgrotesk text-slate-800 hover:text-slate-500">Spineless Laugh</div>
+          <router-link id="logo" class="group flex flex-row" to="/" aria-label="Spineless Laugh">
+            <img src="../images/logo-gradient.svg" id="glyph" class="w-14 transition duration-500 ease-in-out delay-150 hover:rotate-180" alt="Logo" />
+            <div id="logotype" class="text-3xl pl-4 pt-2 transition duration-500 ease-in-out font-bold font-hkgrotesk text-slate-800 hover:text-slate-500">Spineless Laugh</div>
             <!-- <div class="text-4xl pl-4 pt-2 transition duration-500 ease-in-out font-bold font-hkgrotesk text-slate-800 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-teal-500 hover:via-sky-500 hover:to-pink-500">Spineless Laugh</div> -->
           </router-link>
         </div>
